bde48c66338fe08c565d57214ddb672ab13d7c9e
Ricardo Graña <r1k4rd0_0ocho@hotmail.com>
2024-03-27 17:51:22 -0300

    hotfix 5

--- src/routes/views.router.js
+++ src/routes/views.router.js
@@ -1,4 +1,4 @@
-import {Router} from 'express';
+import { Router } from 'express';
 import productController from '../controllers/products.controller.js';
 import sessionController from '../controllers/sessions.controller.js';
 import userController from '../controllers/users.controller.js'
@@ -6,40 +6,40 @@ import { verifyRole } from '../middlewares/verifyRole.js';
 const router = Router();
 
 
-router.get('/home', (req, res)=>{
+router.get('/home', (req, res) => {
     res.render('home');
 });
 
 //esta vista muestra los productos de lista, más el nombre de usuario y más:
 router.get('/productlist', productController.getAllSimple);
 
-router.get('/productlist', (req, res)=>{
-    res.render('productlist', { user: req.session.passport.user })
+router.get('/productlist', (req, res) => {
+    res.render('productlist', { user: req.session.passport.user }, { products: productsDetail })
 })
 
-router.get('/register', (req, res)=>{
+router.get('/register', (req, res) => {
     res.render('register');
 })
-router.get('/register-success', (req, res)=>{
+router.get('/register-success', (req, res) => {
     res.render('registersuccess')
 })
 
-router.get('/realtimeproducts',verifyRole, productController.getProductsRealTime);
+router.get('/realtimeproducts', verifyRole, productController.getProductsRealTime);
 
-router.post('/realtimeproducts',productController.createProductsRealTime);
+router.post('/realtimeproducts', productController.createProductsRealTime);
 
-router.get('/registererror', (req, res)=>{
+router.get('/registererror', (req, res) => {
     res.render('registererror')
 })
-router.get('/errorlogin', (req, res)=>{
+router.get('/errorlogin', (req, res) => {
     res.render('errorlogin')
 })
 router.get('/current', sessionController.profileResponse);
 
-router.get('/profile', (req, res)=>{
-    res.render('profile', {user: req.session.passport.dto});
+router.get('/profile', (req, res) => {
+    res.render('profile', { user: req.session.passport.dto });
 })
-router.get('/ticket', (req, res)=>{
+router.get('/ticket', (req, res) => {
     res.render('ticket')
 })
 
